6c028243c439dfab6038eb90fd6f7c4a3f3482d1
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-10-08 19:35:06 +0200

    initial level generated

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
 function init () {
+
+class sprite {
+  constructor(class, image) {
+    this.type = type
+    this.image = image
+  }
+}
+
+
   const thegrid = document.querySelector('#grid')
   let height = 0
   let width = 0
@@ -6,9 +15,10 @@ function init () {
   // let numCells = height * width
   const cells = []
   let level = 0
-
+  const levelOne = [42,43,62,63,47,46,67,66,56,57,76,77,52,53,72,73,45,65,54,74,29,30,49,50,69,70,102,103,141,161,142,143,162,163,105,125,145,165,117,116,114,134,154,174,156,157,158,176,177,178,107,108,109,110,111,112,129,130,149,150,146,147,166,167,152,153,172,173]
   const startscreen = document.querySelector('.startpage')
   const startbutton = document.querySelector('#start p')
+  let score = 0
   
 
 
@@ -35,9 +45,20 @@ function init () {
         console.log((cell.id))
       } else if (parseFloat(cell.id) + width >= width * width - 1) {
         cell.classList.add('wall')
+      } else if (levelOne.includes(parseFloat(cell.id))){
+        cell.classList.add('wall')
+      } else if (levelOne.map(item => cells.length - 1 - item).includes(parseFloat(cell.id))){
+        cell.classList.add('wall')
+      } else if([189,190,209,210,201,202,203,216,217,218,212,213,214,205,206,207].includes(parseFloat(cell.id))) {
+        cell.classList.add('wall')
       }
     })
+
     
+  }
+
+  const keyHandler = (e) => {
+    console.log(e)
   }
 
   const initiate = () => {
@@ -53,6 +74,8 @@ function init () {
 
 
   startbutton.addEventListener('click',initiate)
+
+  window.addEventListener('keydown',keyHandler)
 
 }
 
